e42b03ce9424a670e43836d4c4fa93ec1806d18c
Avi Cohen <cohena100@gmail.com>
2015-05-02 14:58:50 +0300

    Added self sizing cells and dynamic fonts to the main table view controller.

---
+++ Connection!/MainTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainTableViewCell.swift
+//  Connection!
+//
+//  Created by Avi Cohen on 5/2/15.
+//  Copyright (c) 2015 Avi Cohen. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var actionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
--- Connection!/MainTableViewController.swift
+++ Connection!/MainTableViewController.swift
@@ -10,8 +10,22 @@ import UIKit
 
 class MainTableViewController: UITableViewController {
     
+    deinit {
+        removeNotifications()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 43
+        addNotifications()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        // not my bug: the following code is needed because of a bug that doesn't size the cells correctly on first launch
+        // http://www.appcoda.com/self-sizing-cells/
+        tableView.reloadData()
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -23,6 +37,20 @@ class MainTableViewController: UITableViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     }
     
+    // MARK: Notifications
+    
+    func addNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "didChangePreferredContentSize:", name: UIContentSizeCategoryDidChangeNotification, object: nil)
+    }
+    
+    func removeNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIContentSizeCategoryDidChangeNotification, object: nil)
+    }
+    
+    func didChangePreferredContentSize(notification: NSNotification) {
+        tableView.reloadData()
+    }
+    
 }
 
 // MARK: - UITableViewDelegate
@@ -30,6 +58,23 @@ class MainTableViewController: UITableViewController {
 extension MainTableViewController: UITableViewDelegate {
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    }
+    
+}
+
+// MARK: - UITableViewDataSource
+
+extension MainTableViewController: UITableViewDataSource {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! MainTableViewCell
+        cell.actionLabel.text = NSLocalizedString("Invite a contact from your address book by sms", comment: "tap here to invite a contact from your address book by sms")
+        //not my bug: although defined in interface builder, when change preferred content size notification is received then font needs to be set again
+        cell.actionLabel.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
+        return cell
     }
     
 }
