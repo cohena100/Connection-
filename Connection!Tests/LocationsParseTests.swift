47ef698e41a4992fad5025c9d1f3120eaa5597bb
Avi Cohen <cohena100@gmail.com>
2015-05-30 13:25:32 +0300

    added add enter location using Parse tests.

--- Connection!Tests/LocationsParseTests.swift
+++ Connection!Tests/LocationsParseTests.swift
@@ -15,20 +15,14 @@ class LocationsParseTests: XCTestCase {
     let locationName1 = "locationName1"
     let latitude1 = 1.0
     let longitude1 = 1.0
-    let lid1 = "lid1"
     let name1 = "name1"
     let phone1 = "phone1"
-    let vn1 = "vn1"
-    let cid1 = "cid1"
     
     let locationName2 = "locationName2"
     let latitude2 = 2.0
     let longitude2 = 2.0
-    let lid2 = "lid2"
     let name2 = "name2"
     let phone2 = "phone2"
-    let vn2 = "vn2"
-    let cid2 = "cid2"
 
     let timeout = 10.0
     
@@ -56,4 +50,62 @@ class LocationsParseTests: XCTestCase {
         super.tearDown()
     }
 
+    func testAddEnterLocation_anEnterLocation_enterLocationAdded() {
+        let expectation = expectationWithDescription("invite user")
+        connections.invite(name: name1, phone: phone1, success: { (connection) -> () in
+                expectation.fulfill()
+            }) { (error) -> () in
+                XCTFail("should not end up with an error")
+        }
+        waitForExpectationsWithTimeout(timeout) { [weak self] (error) in
+            if let error = error {
+                XCTFail("should not end up with an error")
+                return
+            }
+            var allConnections: [Connection]?
+            let expectation = self!.expectationWithDescription("get connections")
+            self!.connections.getConnections(success: { (connections) -> () in
+                allConnections = connections
+                expectation.fulfill()
+            }, fail: { (error) -> () in
+                XCTFail("should not end up with an error")
+            })
+            self!.waitForExpectationsWithTimeout(self!.timeout) { [weak self] (error) in
+                if let error = error {
+                    XCTFail("should not end up with an error")
+                    return
+                }
+                let expectation = self!.expectationWithDescription("add enter location")
+                self!.locations.addEnterLocation(name: self!.name1, latitude: self!.latitude1, longitude: self!.longitude2, connections: allConnections!, success: { (location) -> () in
+                    expectation.fulfill()
+                }, fail: { (error) -> () in
+                    XCTFail("should not end up with an error")
+                })
+                self!.waitForExpectationsWithTimeout(self!.timeout) { [weak self] (error) in
+                    if let error = error {
+                        XCTFail("should not end up with an error")
+                        return
+                    }
+                    let allLocations = self!.locations.getLocations()
+                    XCTAssertEqual(allLocations.count, 1, "there should be only one location")
+                    let expectation = self!.expectationWithDescription("cleanup")
+                    self!.connections.deleteLastConnection(
+                        success: { (connection) -> () in
+                            expectation.fulfill()
+                        },
+                        fail: { (error) -> () in
+                            XCTFail("should not end up with an error")
+                        }
+                    )
+                    self!.waitForExpectationsWithTimeout(self!.timeout) { (error) in
+                        if let error = error {
+                            XCTFail("should not end up with an error")
+                            return
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
 }
